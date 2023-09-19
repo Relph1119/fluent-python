3fc5c9e5e04609dd99285237ebd70b78e93c77d4
Relph1119 <huruifeng1202@163.com>
2023-09-19 21:32:46 +0800

    :tada: 学习第9章装饰器和闭包

--- assets/09-closure-deco/clock/clockdeco_param.py
+++ assets/09-closure-deco/clock/clockdeco_param.py
@@ -14,9 +14,10 @@ import time
 
 DEFAULT_FMT = '[{elapsed:0.8f}s] {name}({args}) -> {result}'
 
+
 def clock(fmt=DEFAULT_FMT):  # <1>
-    def decorate(func):      # <2>
-        def clocked(*_args): # <3>
+    def decorate(func):  # <2>
+        def clocked(*_args):  # <3>
             t0 = time.perf_counter()
             _result = func(*_args)  # <4>
             elapsed = time.perf_counter() - t0
@@ -25,14 +26,17 @@ def clock(fmt=DEFAULT_FMT):  # <1>
             result = repr(_result)  # <6>
             print(fmt.format(**locals()))  # <7>
             return _result  # <8>
+
         return clocked  # <9>
+
     return decorate  # <10>
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     @clock()  # <11>
     def snooze(seconds):
         time.sleep(seconds)
+
 
     for i in range(3):
         snooze(.123)
